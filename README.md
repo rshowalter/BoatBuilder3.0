256f06856425c8055077caba95a6eba634201824
sc1341 <33675337+sc1341@users.noreply.github.com>
2019-03-01 13:09:13 -0600

    Update and partial completion with username/api/leaderboard handler
    general update and orginization with classes, to finish my API updating the leaderboard I need the score system build and a datastructure to hold the information that will be sent out to the server. I also included my backend files I created using cloud9. I have my webserver running apache2 and it just holds the small amount of php code that will parse the data and recieve it from the user. I also added lots of comments to explain changes as I went, throughout the code I have a few placeholders for testing purposes which will be removed when we have legit data to send to the website.

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-# BoatBuilder3.0
+# BoatBuilder 3.0
 Below are the files for the BoatBuilder 2.0. Please feel free to edit them and improve this version of boat builder as you wish. 
 
-    (                 )     (             (   (                   )       ) 
+
+   (                 )     (             (   (                   )       ) 
  ( )\          )  ( /(   ( )\    (   (   )\  )\ )   (   (     ( /(    ( /( 
  )((_)  (   ( /(  )\())  )((_)  ))\  )\ ((_)(()/(  ))\  )(    )(_))   )\())
 ((_)_   )\  )(_))(_))/  ((_)_  /((_)((_) _   ((_))/((_)(()\  ((_)    ((_)\ 
  | _ ) ((_)((_)_ | |_    | _ )(_))(  (_)| |  _| |(_))   ((_) |_  )   /  (_)
  | _ \/ _ \/ _` ||  _|   | _ \| || | | || |/ _` |/ -_) | '_|  / /  _| () | 
  |___/\___/\__,_| \__|   |___/ \_,_| |_||_|\__,_|\___| |_|   /___|(_)\__/  
- 
 
 1.Click designated material.
 2. Click designated boat
@@ -19,3 +19,26 @@ _/  /_//_  ___ |  /  / /
 /_____/ /_/  |_/_/  /_/   /
                            a wild boat has appeared
 	                                                                 
+
+
+
+# LeaderBoard
+
+The boat builder leaderboard is accessed through cloud9 using the python requests module
+
+# Added Features and edits 3/1/19
+
+sc1341 edits:
+1. Added feature that allows username to be displayed to the screen
+2. Added drop down menu that links to the github as well as the leaderboard and the not finished name you boat feature
+3. Started the back-end web API for the score system
+4. 
+
+
+# Known errors
+1. instance error with the name your boat feature, we need to figure out how to 
+
+
+# API functionality
+
+The api is run on my cloud 9 account that runs a webserver which allows get requests to add data to the leaderboard, as well as pull data from the existing leaderboard data. It is important to note that I use GET requests and do not use any POST requests for the data, All Back end files are attached to this repository under the backend folder. The leaderboard.json file is where the leaderboard data is held, when pulled down by Python requests, the data is formatted into a dict inside of a list. Visit the https://boatbuilder-api-sc1341.c9users.io website to learn more about the API
--- boatbuilder.py
+++ boatbuilder.py
@@ -1,52 +1,66 @@
-#Imports and a time stopper
+#Boat Builder 3.0 sc1341
 from tkinter import  *
 import requests
 import webbrowser
-import logging
+from getpass import getuser
+
 
 class BoatBuilderGUI:
     
     def __init__(self):
+        #Don't worry about these variables for a while...
+        self.boat_namer_menu = NameBoatMenu()
         #Main root variable
         self.root = Tk()
+        self.root.title("Boat Builder 3.0!")
+        self.root.geometry('600x600')
         self.material = IntVar()
         self.type = IntVar()
         self.canvas = Canvas(self.root, height=600, width=600, bg='#FFFFFF')
         self.API_Handler = APIHandler()
 
         #Creates buttons
-        self.wood_radio = Radiobutton(self.root, text='Wood', variable=self.material, value=1, command = self.wood)
-        self.iron_radio = Radiobutton(self.root, text='Iron', variable=self.material, value=2, command = self.iron)
-        self.steel_radio = Radiobutton(self.root, text= 'Steel', variable=self.material, value=3, command = self.steel)
+        self.wood_radio = Radiobutton(self.root, text='Wood', variable=self.material, value=1, command=self.wood)
+        self.iron_radio = Radiobutton(self.root, text='Iron', variable=self.material, value=2, command=self.iron)
+        self.steel_radio = Radiobutton(self.root, text= 'Steel', variable=self.material, value=3, command=self.steel)
         self.sailboat_radio = Radiobutton(self.root, text='Sailboat, "Caution may break upon use"', variable=self.type, value=1, command=self.sailboat)
-        self.ironclad_radio = Radiobutton(self.root, text='Ironclad, meh', variable=self.type, value=2, command = self.ironclad)
+        self.ironclad_radio = Radiobutton(self.root, text='Ironclad, meh', variable=self.type, value=2, command=self.ironclad)
         self.warship_radio = Radiobutton(self.root, text='Warship, "Boat Builder Destroyer"', variable=self.type, value=3, command=self.warship)
-
+        #Main label 
+        self.main_label = Label(self.root, text=str(getuser()) + "'s boat!")
 
         #NEW ADDED FEATURE WITH LEADERBOARD DROP DOWN MENU
         self.menu_bar = Menu(self.root)
         self.root.config(menu=self.menu_bar)
 
+        #Catches window closing and saves data
+        self.root.bind("<Destroy>", self.API_Handler.post_data_to_leaderboard('test user','test user'))
+
         #Define both of the variables that hold the drop downs for each submenu in the top bar
         self.leaderboardmenu = Menu(self.menu_bar)
         self.sharemenu = Menu(self.menu_bar)
+        self.nameyourboatmenu = Menu(self.menu_bar)
 
         #Cascade for the leaderboard menu
         self.menu_bar.add_cascade(label="Leaderboard", menu=self.leaderboardmenu)
         self.leaderboardmenu.add_command(label="View Leaderboard", command=self.leaderboard_view_command)
         #Cascade for the Share menu
         self.menu_bar.add_cascade(label="Share Boatbuilder!", menu=self.sharemenu)
-        self.sharemenu.add_command(label="View on github")
+        self.sharemenu.add_command(label="View on GitHub", command=self.share_boat_builder)
+        #Cascade for the nameyourboatmenu
+        self.menu_bar.add_cascade(label="Name your Boat!", menu=self.nameyourboatmenu)
+        self.nameyourboatmenu.add_command(label="Click me to name your boat!", command=self.name_boat)
 
+        #main lable grid
+        self.main_label.grid(row=0, column=4, sticky=N)
 
         #Sets up positions for buttons
-        self.wood_radio.grid(row=0, column = 0, sticky=W)
-        self.iron_radio.grid(row=0, column = 4, sticky=W)
-        self.steel_radio.grid(row=0, column=8, sticky=W)
-        self.sailboat_radio.grid(row=1, column=0, sticky=W)
-        self.ironclad_radio.grid(row=1, column=4, sticky=W)
-        self.warship_radio.grid(row=1, column=8, sticky=W)
-        
+        self.wood_radio.grid(row=1, column = 0, sticky=W)
+        self.iron_radio.grid(row=1, column = 4, sticky=W)
+        self.steel_radio.grid(row=1, column=8, sticky=W)
+        self.sailboat_radio.grid(row=2, column=0, sticky=W)
+        self.ironclad_radio.grid(row=2, column=4, sticky=W)
+        self.warship_radio.grid(row=2, column=8, sticky=W)
         self.canvas.grid(row=10,rowspan=999, column = 0, columnspan = 12)
         #creates colors
         self.red_intvar = IntVar()
@@ -68,12 +82,14 @@ class BoatBuilderGUI:
         pass
 
     def sailboat(self):
+        '''Creates the sailboat rectangle and oval'''
         self.canvas.delete("all")
         rectangle1 = self.canvas.create_rectangle(100, 100, 200, 200, fill="#850000", outline = "#850000") 
         circle1 = self.canvas.create_oval(100, 140, 500, 500, fill='#850000', outline = "#850000")
 
 
     def ironclad(self):
+        '''Creates the ironclad rectangle and oval'''
         self.canvas.delete("all")
         rectangle = self.canvas.create_rectangle(200, 50, 100, 150, fill="#ece9ec", outline='#ece9ec')
         circle = self.canvas.create_oval(100, 140, 500, 500, fill="#ece9ec", outline = "#ece9ec")
@@ -99,16 +115,51 @@ class BoatBuilderGUI:
         l = Leaderboard(self.API_Handler.get_leaderboard())
 
     def share_boat_builder(self):
+        '''Launches a new window in the browser to the github page for boatbuilder 3.0'''
         webbrowser.open_new_tab("https://github.com/rshowalter/BoatBuilder3.0")
-        
+
+    def name_boat(self):
+        '''Allows the user to name the boat, this calls the instance of the class created in the __init__ method,
+        this code isn't great for this but idk how else to do it at the moment'''
+        #self.boat_namer_menu.name_your_boat_menu()
+        pass
+
+class NameBoatMenu:
+    '''this does not work due to instance of class errors temp solution with getpass'''
+    def __init__(self):
+        pass
+
+    def name_your_boat_menu(self):
+        self.root = Tk()
+        self.root.title("Name your boat!")
+        self.root.geometry("300x300")
+        #self.root.config(bg="blue")
+        #Varibale that holds the boat name intitially
+        self.boat_string = StringVar()
+        #Widgets
+        self.entry = Entry(self.root, textvariable=self.boat_string)
+        #Yikes ok, so this lambda is run to grab the name of the boat, this is so bad and I can't think of any 
+        #other solutions so we are going to leave it for now
+        #grabname = lambda: name = self.boat_name_submit; return name
+        self.submit_button = Button(self.root, text="Submit name", command=self.boat_name_submit)
+
+        #grid the widgets in place
+        self.entry.grid(row=0, column=0)
+        self.submit_button.grid(row=2, column=0)
+        #root mainloop
+        self.root.mainloop()
+    
+    def boat_name_submit(self):
+        '''Returns the name of the boat from the entry'''
+        self.boat_name = self.entry.get()
+        #Idk if i need this or not yet I can't remember
+        self.boat_name = str(self.boat_name)
+        return self.boat_name
         
 class APIHandler:
     
-    def __init__(self):
-        pass
-    
     def get_leaderboard(self):
-        '''Gets the leaderboard data from the API'''
+        '''Gets the leaderboard data from the API by making a GET request to the leaderboard.json file'''
         try:
             r = requests.get('https://boatbuilder-api-sc1341.c9users.io/leaderboard.json')
             return r.json()
@@ -119,7 +170,7 @@ class APIHandler:
         '''Puts the user data into the leaderboard on the API side, leaderboard data should be a dictionary
         this does use a get request FYI, it is ok that values are put through url in this case'''
         r = requests.get('https://boatbuilder-api-sc1341.c9users.io/leaderboard.php?playername=' + playername + '&boattype=' + boattype)
-    
+        
     
 
 class Leaderboard:
@@ -130,14 +181,15 @@ class Leaderboard:
         self.root.title("Boatbuilder Leaderboard!")
 
         #For loop that puts the data into rows and grids them on the screen
-        height = 3
-        width = 1
+        height = 0
+        width = 0
         for key, value in leaderboarddata[0].items():
-            for i in range(height):
-                for j in range(width): #Columns
-                    b = Label(self.root, text=key + " " + value)
-                    b.grid(row=i, column=j)
+            height += 1
+            b = Label(self.root, text=key + " " + value)
+            b.grid(row=height, column=width)
+
         self.root.mainloop()
+
 
 if __name__ == '__main__':
     gui = BoatBuilderGUI()
