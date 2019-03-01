3bf22c0f946277c7c938b7c28da23bcb2a54afe0
sc1341 <33675337+sc1341@users.noreply.github.com>
2019-02-28 22:49:28 -0600

    added menus and cleaned code by creating classes, I also started the API backend code
    cool stuff

--- PLTW project.py
+++ PLTW project.py
@@ -1,20 +1,106 @@
-#Imports Tkinter and a time stopper
-import tkinter
-import time
+#Imports and a time stopper
+from tkinter import  *
 import requests
-
+import webbrowser
 
 #Class test
 
-class GUI:
+class BoatBuilderGUI:
     
     def __init__(self):
-        self.root = tkinter.Tk()
-        self.material = tkinter.IntVar()
-        self.type = tkinter.IntVar()
-        self.canvas = tkinter.Canvas(root, height=600, width=600, bg='#FFFFFF')
+        #Main root variable
+        self.root = Tk()
+        self.material = IntVar()
+        self.type = IntVar()
+        self.canvas = Canvas(self.root, height=600, width=600, bg='#FFFFFF')
         self.API_Handler = APIHandler()
+
+        #Creates buttons
+        self.wood_radio = Radiobutton(self.root, text='Wood', variable=self.material, value=1, command = self.wood)
+        self.iron_radio = Radiobutton(self.root, text='Iron', variable=self.material, value=2, command = self.iron)
+        self.steel_radio = Radiobutton(self.root, text= 'Steel', variable=self.material, value=3, command = self.steel)
+        self.sailboat_radio = Radiobutton(self.root, text='Sailboat, "Caution may break upon use"', variable=self.type, value=1, command=self.sailboat)
+        self.ironclad_radio = Radiobutton(self.root, text='Ironclad, meh', variable=self.type, value=2, command = self.ironclad)
+        self.warship_radio = Radiobutton(self.root, text='Warship, "Boat Builder Destroyer"', variable=self.type, value=3, command=self.warship)
+
+
+        #NEW ADDED FEATURE WITH LEADERBOARD DROP DOWN MENU
+        self.menu_bar = Menu(self.root)
+        self.root.config(menu=self.menu_bar)
+
+        #Define both of the variables that hold the drop downs for each submenu in the top bar
+        self.leaderboardmenu = Menu(self.menu_bar)
+        self.sharemenu = Menu(self.menu_bar)
+
+        #Cascade for the leaderboard menu
+        self.menu_bar.add_cascade(label="Leaderboard", menu=self.leaderboardmenu)
+        self.leaderboardmenu.add_command(label="View Leaderboard", command=self.leaderboard_view_command)
+        #Cascade for the Share menu
+        self.menu_bar.add_cascade(label="Share Boatbuilder!", menu=self.sharemenu)
+        self.sharemenu.add_command(label="View on github")
+
+
+        #Sets up positions for buttons
+        self.wood_radio.grid(row=0, column = 0, sticky=W)
+        self.iron_radio.grid(row=0, column = 4, sticky=W)
+        self.steel_radio.grid(row=0, column=8, sticky=W)
+        self.sailboat_radio.grid(row=1, column=0, sticky=W)
+        self.ironclad_radio.grid(row=1, column=4, sticky=W)
+        self.warship_radio.grid(row=1, column=8, sticky=W)
         
+        self.canvas.grid(row=10,rowspan=999, column = 0, columnspan = 12)
+        #creates colors
+        self.red_intvar = IntVar()
+        self.green_intvar = IntVar()
+        self.blue_intvar = IntVar()
+
+        #Don't know why this is here
+        self.material.set(1) 
+        self.type.set(1)
+        #self.root mainloop
+        self.root.mainloop()
+
+    def hexstring(self, slider_intvar):
+        '''A function to prepare data from controller's widget for view's consumption
+        
+        slider_intvar is an IntVar between 0 and 255, inclusive
+        hexstring() returns a string representing two hexadecimal digits
+        '''
+        pass
+
+    def sailboat(self):
+        self.canvas.delete("all")
+        rectangle1 = self.canvas.create_rectangle(100, 100, 200, 200, fill="#850000", outline = "#850000") 
+        circle1 = self.canvas.create_oval(100, 140, 500, 500, fill='#850000', outline = "#850000")
+
+
+    def ironclad(self):
+        self.canvas.delete("all")
+        rectangle = self.canvas.create_rectangle(200, 50, 100, 150, fill="#ece9ec", outline='#ece9ec')
+        circle = self.canvas.create_oval(100, 140, 500, 500, fill="#ece9ec", outline = "#ece9ec")
+
+        
+    def warship(self):    
+        self.canvas.delete("all")
+        rectangle2 = self.canvas.create_rectangle(100, 100, 200, 200, fill='#c3cbad', outline='#c3cbad')
+        rectangle3 = self.canvas.create_rectangle(150, 150, 250, 200, fill='#c3cbad', outline='#c3cbad')
+        circle2 = self.canvas.create_oval(100, 140, 500, 500, fill='#c3cbad', outline='#c3cbad')
+
+    def wood(self):
+        pass
+
+    def steel(self):
+        pass
+    def iron(self):
+        pass
+
+    #Drop down menu functions
+    def leaderboard_view_command(self):
+        '''Displays the leaderboard from the menu'''
+        l = Leaderboard(self.API_Handler.get_leaderboard())
+
+    def share_boat_builder(self):
+        webbrowser.open_new_tab("https://github.com/rshowalter/BoatBuilder3.0")
         
         
 class APIHandler:
@@ -23,93 +109,34 @@ class APIHandler:
         pass
     
     def get_leaderboard(self):
-        ''''''
-        r = requests.get("http://")
-        return r.text
+        '''Gets the leaderboard data from the API'''
+        try:
+            r = requests.get('https://boatbuilder-api-sc1341.c9users.io/leaderboard.json')
+            return r.json()
+        except:
+            return {"status":"api error"}
     
-    def post_data_to_leaderboard(self, leaderboarddata):
-        ''''''
-        pass
+    def post_data_to_leaderboard(self, playername, boattype):
+        '''Puts the user data into the leaderboard on the API side, leaderboard data should be a dictionary
+        this does use a get request FYI, it is ok that values are put through url in this case'''
+        r = requests.get('https://boatbuilder-api-sc1341.c9users.io/leaderboard.php?playername=' + playername + '&boattype=' + boattype)
     
-    
-        
-    
-    
-
-#Makes a shortcut
-root = tkinter.Tk()
-#Sets up Variables
-# 'Most recent' to know which radio button is 'on'
-Material = tkinter.IntVar()
-Type = tkinter.IntVar()
-#Makes a canvas
-canvas = tkinter.Canvas(root, height = 600, width = 600, bg='#FFFFFF')
-canvas.grid(row=10,rowspan=999, column = 0, columnspan = 12)
-#creates colors
-red_intvar = tkinter.IntVar()
-green_intvar = tkinter.IntVar()
-blue_intvar = tkinter.IntVar()
-startx , starty = 300, 300
-for x in  range(10, 300, 40):
-    y = x
-    r = 30 #So all circles will have this radius
     
 
-    
-def hexstring(slider_intvar):
-    '''A function to prepare data from controller's widget for view's consumption
-    
-    slider_intvar is an IntVar between 0 and 255, inclusive
-    hexstring() returns a string representing two hexadecimal digits
-    '''
+class Leaderboard:
 
-def Sailboat():
-    canvas.delete("all")
-    y = x # So that circles are along diagonal line y=x 
-    r = 30 # All the circles will have this radius
-    rectangle1 = canvas.create_rectangle(100, 100, 200, 200, fill="#850000", outline = "#850000") 
-    circle1 = canvas.create_oval(100, 140, 500, 500, fill='#850000', outline = "#850000")
+    def __init__(self, leaderboarddata):
+        self.root = Tk()
+        data = leaderboarddata
+        height = 3
+        width = 2
+        for key, value in data.items():
+            for i in range(height):
+                for j in range(width): #Columns
+                    b = Label(self.root, text=key + " " + value)
+                    b.grid(row=i, column=j)
 
+        self.root.mainloop()
 
-def Ironclad():
-    canvas.delete("all")
-    y = x # So that circles are along diagonal line y=x 
-    r = 30 # All the circles will have this radius
-    rectangle = canvas.create_rectangle(200, 50, 100, 150, fill="#ece9ec", outline='#ece9ec')
-    circle = canvas.create_oval(100, 140, 500, 500, fill="#ece9ec", outline = "#ece9ec")
-
-    
-def Warship():    
-    canvas.delete("all")
-    y = x # So that circles are along diagonal line y=x 
-    r = 30 # All the circles will have this radius
-    rectangle2 = canvas.create_rectangle(100, 100, 200, 200, fill='#c3cbad', outline='#c3cbad')
-    rectangle3 = canvas.create_rectangle(150, 150, 250, 200, fill='#c3cbad', outline='#c3cbad')
-    circle2 = canvas.create_oval(100, 140, 500, 500, fill='#c3cbad', outline='#c3cbad')
-
-#Makes buttons
-Wood_Radio = tkinter.Radiobutton(root, text='Wood', variable=Material, value=1, command = wood)
-Iron_Radio = tkinter.Radiobutton(root, text='Iron', variable=Material, value=2, command = iron)
-Steel_Radio = tkinter.Radiobutton(root, text= 'Steel', variable = Material, value=3, command = steel)
-Sailboat_Radio = tkinter.Radiobutton(root, text='Sailboat, "Caution may break upon use"', variable=Type, value=1, command = Sailboat)
-Ironclad_Radio = tkinter.Radiobutton(root, text='Ironclad, meh', variable=Type, value=2, command = Ironclad)
-Warship_Radio = tkinter.Radiobutton(root, text='Warship, "Boat Builder Destroyer"', variable=Type, value=3, command = Warship)
-
-
-#Sets up positions for buttons
-Wood_Radio.grid(row=0, column = 0, sticky=tkinter.W)
-Iron_Radio.grid(row=0, column = 4, sticky=tkinter.W)
-Steel_Radio.grid(row=0, column=8, sticky=tkinter.W)
-Sailboat_Radio.grid(row=1, column=0, sticky=tkinter.W)
-Ironclad_Radio.grid(row=1, column=4, sticky=tkinter.W)
-Warship_Radio.grid(row=1, column=8, sticky=tkinter.W)
-
-
-#Sets Radio Buttons to 1, so only one is activated when program started.
-#Otherwise all butons would be "On"
-Material.set(1) 
-Type.set(1)
-
-
-#Event loop, loops everything
-root.mainloop()
+if __name__ == '__main__':
+    gui = BoatBuilderGUI()
